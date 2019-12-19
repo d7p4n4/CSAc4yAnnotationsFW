55fdbde9ac0c29b5b11aa42a008887d8af923a13
Zsombor Bérces <32767685+d7p4n4@users.noreply.github.com>
2019-12-19 15:34:29 +0100

    Add project files.

---
+++ CSAc4yAnnotationsFW/Ac4yWidgetType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAc4yAnnotationsFW
+{
+    class Ac4yWidgetType : Ac4yAnnotation
+    {
+        public string Type { get; set; }
+    }
+}
